dd7558ec04390f9e1464c415dccb0c115c0f0d31
andrewadams32 <andrewadams3@mail.fresnostate.edu>
2019-09-13 20:37:45 -0700

    mongo connected, routes made

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config();
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var locationRouter = require('./routes/location');
+var battleRouter = require('./routes/battle');
 
 var app = express();
 
@@ -16,5 +21,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/location', locationRouter);
+app.use('/battle', battleRouter);
+
+
+//Mongo Setup
+var url = process.env.mongo_url
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .catch( (err) => {
+    console.log("mongo error", err);
+  })
+mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify', false);
 
 module.exports = app;
---
+++ schemas/userSchema.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+var UserSchema = new Schema({
+  userId: {
+    type: ObjectId,
+  },
+  username: {
+      type: String,
+      require: true
+  },
+  password: {
+      type:String,
+      required:true
+  },
+  dateCreated:{
+      type: Date,
+      required: true
+  }
+});
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports=Assignment;
