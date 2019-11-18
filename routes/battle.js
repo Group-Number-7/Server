6431bcdd7f5e2742042b923c860a6c80b6e32e96
aizaz99 <aizaz1996@mail.fresnostate.edu>
2019-11-18 12:01:35 -0800

    enemyhealthcondition

--- routes/battle.js
+++ routes/battle.js
@@ -1,8 +1,39 @@
 var express = require('express');
 var router = express.Router();
+const Enemy = require('../schemas/enemySchema');
 
 router.get('/', (req, res)=> {
     res.send({ok: "battle"})
 })
 
 module.exports = router;
+
+ router.post('/attack/:id', function(req, res, next) {
+    const amount = req.amount;
+    Enemy.findById(req.params.id).then((enemy)=>{
+        if(enemy){
+           const newlife = enemy.stats.hp - amount
+            if(newlife <= 0 ) {
+                Enemy.findByIdAndDelete(req.params.id)
+                res.send(1)
+            }  else {
+                enemy.stats.hp = newlife
+                enemy.save();
+                res.send(0)
+
+
+            }
+
+        } else {
+//enemy not found
+        res.send(-1)
+        }
+    }, (err)=>{
+// error occured finding enemy
+    res.send(-1)
+    })
+ })
+
+
+
+
